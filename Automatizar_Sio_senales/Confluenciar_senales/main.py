d163436d3ab4a48cb802962ff3b5078922581f42
Kevin <andrestorrecilla.14@gmail.com>
2023-11-14 11:38:21 -0500

    refactor(run(), obtener_cantidad_repeticiones()):Modularizando contenido de funciones; chore(operator): eliminando modulo

--- Automatizar_Sio_senales/Confluenciar_senales/main.py
+++ Automatizar_Sio_senales/Confluenciar_senales/main.py
@@ -1,45 +1,52 @@
 from os import path, walk, makedirs
 from datetime import datetime
 from sys import path as pth
-from operator import itemgetter
 root = path.dirname(path.dirname(path.abspath(__file__)))
 pth.append(root)
 
 from Directorio import obtener_directorio
 
 
+def obtener_senales_de_archivos(archivo, raiz, lineas):
+    if archivo.endswith('.txt'):
+        ruta_archivo = path.join(raiz, archivo)
+        print(ruta_archivo)
+        with open(ruta_archivo, 'r') as f:
+            lineas_archivo = f.readlines()
+            if len(lineas_archivo) > 1:
+                lineas_archivo = list(map(lambda x: x.replace("\n", ""), lineas_archivo))
+                lineas.extend(lineas_archivo)
+
+
+def contar_y_clasificar_lineas(cant_veces, lista_unica_cant_veces, linea, lineas_M5, lineas_M15, lineas_unicas_filtradas, lista_cant_veces):
+    if cant_veces not in lista_unica_cant_veces:
+            lista_unica_cant_veces.append(cant_veces)
+    if cant_veces > 1:
+        if linea.split(";")[0] == "M5":
+            lineas_M5.append(linea)
+        elif linea.split(";")[0] == "M15":
+            lineas_M15.append(linea)
+        print(f"La linea {linea} se repite {cant_veces} {'veces' if cant_veces > 1 else 'vez'}")
+        lineas_unicas_filtradas.append(linea)
+    lista_cant_veces.append(cant_veces)
+    sorted(lista_unica_cant_veces)
+
+
 def obtener_cantidad_repeticiones(lineas):
     lista_cant_veces = []
     lista_unica_cant_veces = []
-    
     lineas_unicas = tuple(set(lineas))
     lineas_unicas_filtradas = []
     lineas_M5 = []
     lineas_M15 = []
     lineas_unicas = sorted(lineas_unicas, key=lambda x: datetime.strptime(x.split(';')[2], "%H:%M"))
+
     for linea in lineas_unicas:
         cant_veces = lineas.count(linea)
-        if cant_veces not in lista_unica_cant_veces:
-            lista_unica_cant_veces.append(cant_veces)
-        if cant_veces > 1:
-            if linea.split(";")[0] == "M5":
-                lineas_M5.append(linea)
-            elif linea.split(";")[0] == "M15":
-                lineas_M15.append(linea)
-            print(f"La linea {linea} se repite {cant_veces} {'veces' if cant_veces > 1 else 'vez'}")
-            lineas_unicas_filtradas.append(linea)
-        lista_cant_veces.append(cant_veces)
-        sorted(lista_unica_cant_veces)
+        contar_y_clasificar_lineas(cant_veces, lista_unica_cant_veces, lineas, lineas_M5, lineas_M15, lineas_unicas_filtradas, lista_cant_veces)
+
     for i in lista_unica_cant_veces:
         print(f"Las señales de {i} veces repetidas son: {lista_cant_veces.count(i)}")
-        
-    
-    # repeticiones_ordenadas = sorted(repeticiones.items(), key=itemgetter(1))
-    # print(repeticiones_ordenadas)
-    # print(type(repeticiones_ordenadas))
-
-    # for linea, cantidad in repeticiones_ordenadas:
-    #     print(f"{cantidad}. {linea}")
 
     return lineas_M5, lineas_M15, lineas_unicas_filtradas
 
@@ -54,28 +61,18 @@ def run():
 
     for raiz, dirs, archivos in walk(directorio_filtro_backtest):
         for archivo in archivos:
-            if archivo.endswith('.txt'):
-                ruta_archivo = path.join(raiz, archivo)
-                print(ruta_archivo)
-                with open(ruta_archivo, 'r') as f:
-                    lineas_archivo = f.readlines()
-                    if len(lineas_archivo) > 1:
-                        lineas_archivo = list(map(lambda x: x.replace("\n", ""), lineas_archivo))
-                        lineas.extend(lineas_archivo)
+            obtener_senales_de_archivos(archivo, raiz, lineas)
+            
     lineas_M5, lineas_M15, lineas_todas = obtener_cantidad_repeticiones(lineas)
-
     directorio_confluencia = path.join(directorio, "Confluencia_senales")
     makedirs(directorio_confluencia, exist_ok=True)
-    ruta_archivo_M5, ruta_archivo_M15, ruta_archivo_todas = path.join(directorio_confluencia, "Senales_M5.txt"), path.join(directorio_confluencia, "Senales_M15.txt"), path.join(directorio_confluencia, "Senales.txt")
 
-    lineas_M5, lineas_M15, lineas_todas = "\n".join(lineas_M5), "\n".join(lineas_M15), "\n".join(lineas_todas)
+    rutas_archivos = (path.join(directorio_confluencia, "Senales_M5.txt"), path.join(directorio_confluencia, "Senales_M15.txt"), path.join(directorio_confluencia, "Senales.txt"))
+    lineas_senales = "\n".join(lineas_M5), "\n".join(lineas_M15), "\n".join(lineas_todas)
 
-    with open(ruta_archivo_M5, "w") as f:
-        f.write(lineas_M5)
-    with open(ruta_archivo_M15, "w") as f:
-        f.write(lineas_M15)
-    with open(ruta_archivo_todas, "w") as f:
-        f.write(lineas_todas)
+    for ruta_archivo, linea_senal in zip(rutas_archivos, lineas_senales):
+        with open(ruta_archivo, "w") as f:
+            f.write(linea_senal)
 
 
 if __name__ == "__main__":
