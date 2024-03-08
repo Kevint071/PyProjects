d38a9bd2140e8fd755784d8776f3a63e4a98e418
Kevin <andrestorrecilla.14@gmail.com>
2024-03-08 10:20:18 -0500

    feat(new file): Buscar archivo de forma síncrona

---
+++ Buscar_archivo_sincronía.py
@@ -0,0 +1,28 @@
+from os import walk, path
+from time import time
+
+ejemplo_dir = input("Digite el directorio: ")
+palabra = input("Digite la palabra que desea buscar en los archivos: ").lower()
+dir_o_file = int(input("""\n¿Que quiere buscar?:
+
+    1. Archivo
+    2. Carpeta
+            
+    Elige un número:"""))
+
+print("")
+inicio = time()
+for nombre_directorio, directorios, ficheros in walk(ejemplo_dir):
+
+    if dir_o_file == 1:
+        for nombre_fichero in ficheros:
+            if nombre_fichero.lower().count(palabra):
+                print("Directorio: ", nombre_directorio)
+                print(f"Archivo: {nombre_fichero}\n")
+
+    if dir_o_file == 2:
+        for directorio in directorios:
+            if directorio.count(palabra):
+                print(f"{path.join(nombre_directorio, directorio)}\n")
+fin = time()
+print(fin - inicio)
