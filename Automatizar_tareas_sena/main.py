e83aa959d16c95e3fff8dc1f5122e4d5acc8fd5b
Kevin <andrestorrecilla.14@gmail.com>
2024-03-09 00:40:10 -0500

    feat(diseño):avanzando en diseño

--- Automatizar_tareas_sena/main.py
+++ Automatizar_tareas_sena/main.py
@@ -2,21 +2,13 @@ import flet as ft
 
 
 def configuracion_ventana(page: ft.Page):
-    page.title = "Gestor de Evidencias SENA"
+    page.title = "Territorium Flow"
     page.window_width = 1020
     page.window_height = 600
     page.window_center()
     page.window_resizable = False
     page.window_maximizable = False
     page.bgcolor = "#101010"
-
-def configuracion_menu():
-    alignment=ft.alignment.top_left
-    height=20
-    padding = ft.padding.only(left=20)
-    margin = ft.margin.only(top=10)
-
-    return (alignment, height, padding, margin)
 
 
 def main(page: ft.Page):
@@ -41,24 +33,81 @@ def main(page: ft.Page):
                     margin=0,
                     content=ft.Column(controls=[
                         ft.Container(
-                            content=ft.Text("Utilidades", color=ft.colors.GREY_500, weight=ft.FontWeight.W_600),
+                            content=ft.Text("Opciones Automatizadas", color=ft.colors.GREY_600, weight=ft.FontWeight.W_600, size=15),
                             alignment=ft.alignment.top_left ,
                             height=20,
-                            padding = ft.padding.only(left=20),
+                            padding = ft.padding.only(top=0, right=10, bottom=0, left=20),
+                            margin = ft.margin.only(top=10, bottom=-10),
+                           
+                        ),
+                        ft.Container(
+                            content=ft.Text("Gestión de Evidencias", color=ft.colors.BLACK87, weight=ft.FontWeight.W_700, size=20),
+                            alignment=ft.alignment.top_left,
+                            height=25,
+                            padding = ft.padding.only(left=20, top=0),
                             margin = ft.margin.only(top=10),
                         ),
                         ft.Container(
-                            content=ft.Text("Automatización", color=ft.colors.BLACK38, weight=ft.FontWeight.W_700),
-                            alignment=ft.alignment.top_left ,
-                            height=20,
-                            padding = ft.padding.only(left=20),
+                            content=ft.Row([ft.Icon(name=ft.icons.ADD_TASK),ft.Text("Publicar Evidencias", color=ft.colors.BLACK87, weight=ft.FontWeight.W_500, size=15)]),
+                            alignment=ft.alignment.top_left,
+                            height=25,
+                            padding = ft.padding.only(left=20, top=0),
                             margin = ft.margin.only(top=10),
+                            on_click=lambda _: print("Publicado!")
                         ),
                         ft.Container(
-                            alignment=ft.alignment.center,
-                            expand=True,
-                            padding=0,
-                            margin=0,
+                            content=ft.Row([ft.Icon(name=ft.icons.QUESTION_MARK),ft.Text("Consultar Evidencias", color=ft.colors.BLACK87, weight=ft.FontWeight.W_500, size=15)]),
+                            alignment=ft.alignment.top_left,
+                            height=25,
+                            padding = ft.padding.only(left=20, top=0),
+                            margin = ft.margin.only(top=5),
+                            on_click=lambda _: print("Consultado!"),
+                        ),
+                        ft.Container(
+                            content=ft.Text("Mensajes", color=ft.colors.BLACK87, weight=ft.FontWeight.W_700, size=20),
+                            alignment=ft.alignment.top_left,
+                            height=25,
+                            padding = ft.padding.only(left=20, top=0),
+                            margin = ft.margin.only(top=20),
+                        ),
+                        ft.Container(
+                            content=ft.Row([ft.Icon(name=ft.icons.SEND),ft.Text("Enviar Mensaje", color=ft.colors.BLACK87, weight=ft.FontWeight.W_500, size=15)]),
+                            alignment=ft.alignment.top_left,
+                            height=25,
+                            padding = ft.padding.only(left=20, top=0),
+                            margin = ft.margin.only(top=10),
+                            on_click=lambda _: print("Mensaje Enviado!"),
+                        ),
+                        ft.Container(
+                            content=ft.Row([ft.Icon(name=ft.icons.CHAT),ft.Text("Ver Conversaciones", color=ft.colors.BLACK87, weight=ft.FontWeight.W_500, size=15)]),
+                            alignment=ft.alignment.top_left,
+                            height=25,
+                            padding = ft.padding.only(left=20, top=0),
+                            margin = ft.margin.only(top=10),
+                            on_click=lambda _: print("Conversación Mostrada"),
+                        ),
+                        ft.Container(
+                            content=ft.Text("Contenido", color=ft.colors.BLACK87, weight=ft.FontWeight.W_700, size=20),
+                            alignment=ft.alignment.top_left,
+                            height=25,
+                            padding = ft.padding.only(left=20, top=0),
+                            margin = ft.margin.only(top=20),
+                        ),
+                        ft.Container(
+                            content=ft.Row([ft.Icon(name=ft.icons.BOOKMARK_BORDER_ROUNDED),ft.Text("Guías de Aprendizaje", color=ft.colors.BLACK87, weight=ft.FontWeight.W_500, size=15)]),
+                            alignment=ft.alignment.top_left,
+                            height=25,
+                            padding = ft.padding.only(left=20, top=0),
+                            margin = ft.margin.only(top=10),
+                            on_click=lambda _: print("Mensaje Enviado!"),
+                        ),
+                        ft.Container(
+                            content=ft.Row([ft.Icon(name=ft.icons.LIBRARY_BOOKS),ft.Text("Material de Formación", color=ft.colors.BLACK87, weight=ft.FontWeight.W_500, size=15)]),
+                            alignment=ft.alignment.top_left,
+                            height=25,
+                            padding = ft.padding.only(left=20, top=0),
+                            margin = ft.margin.only(top=10),
+                            on_click=lambda _: print("Mensaje Enviado!"),
                         ),
                     ]), 
                 ),
@@ -69,6 +118,22 @@ def main(page: ft.Page):
                     expand=True,
                     padding=0,
                     margin=0,
+                    content=ft.Column(controls=[
+                        ft.Container(
+                            content=ft.Text("Territorium Flow", theme_style=ft.TextThemeStyle.HEADLINE_LARGE, text_align=ft.TextAlign.CENTER, color=ft.colors.BLACK87, weight=ft.FontWeight.W_700),
+                            alignment=ft.alignment.center,
+                            height=60,
+                            margin = ft.margin.only(top=30),
+                        ),
+                        ft.Container(
+                            content=ft.Text("Guías de Aprendizaje", color=ft.colors.BLACK87, weight=ft.FontWeight.W_500, size=15),
+                            alignment=ft.alignment.top_left,
+                            height=25,
+                            padding = ft.padding.only(left=20, top=0),
+                            margin = ft.margin.only(top=10),
+                            on_click=lambda _: print("Mensaje Enviado!"),
+                        ),
+                    ])
                 ),
             ],
             spacing=0,
