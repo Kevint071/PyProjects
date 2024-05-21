d2b4db4d928344b134c58c52f8212f4a4e99bc34
Kevin <andrestorrecilla.14@gmail.com>
2024-05-20 21:06:08 -0500

    fix(crear_contenedor_botones): Corrgiendo atributos de las funciones en las lambda

--- Descargar_playlist_youtube/Interfaz_main.py
+++ Descargar_playlist_youtube/Interfaz_main.py
@@ -1,10 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
-from Logica_interfaces import validar_enlace, agregar_url, configurar_botones
+from Logica_interfaces import validar_enlace, configurar_botones
 from Configuracion_interfaces import configuracion_ventana_root, estilos_ventana_root
 from Interfaz_popup import ventana_confirmacion
-from time import sleep
-from threading import Thread
 
 label_info = None
 
@@ -26,6 +24,7 @@ def crear_componentes():
 
     [componente() for componente in componentes]
 
+
 def crear_titulo_principal():
     titulo = ttk.Label(root, text="Descargador de PlayLists de YouTube", anchor="center", style="Title.TLabel")
     titulo.pack(pady=30)
@@ -43,22 +42,10 @@ def crear_contenedor_entry():
     entry_playlist.pack(side=tk.LEFT, padx=10)
 
 
-# mensajes = ["Ejemplo: https://www.youtube.com/playlist?list=PLTVcU6RR9nxvaS4KxRnBteKL2A6T5H0Ko", "Complete el campo con un enlace de playlist de youtube..."]
-# indice_mensaje = 1
-
-
-# def actualizar_mensaje():
-#     global indice_mensaje
-#     label_info.config(text=mensajes[indice_mensaje])
-#     indice_mensaje = (indice_mensaje + 1) % len(mensajes)
-#     root.after(5000, actualizar_mensaje)  # Actualiza el mensaje cada segundo
-
-
 def crear_mensaje_info():
     global label_info
     label_info = ttk.Label(root, text="", style="Info.TLabel")
     label_info.pack(pady=(10, 0))
-    # actualizar_mensaje()        
 
 
 def crear_contenedor_botones():
@@ -68,10 +55,10 @@ def crear_contenedor_botones():
     formato_mp3 = "MP3"
     formato_mp4 = "MP4"
 
-    boton_mp3 = ttk.Button(frame_botones, text="Descargar MP3", command=lambda: (ventana_confirmacion(root, validar_enlace(entry_playlist, label_info), formato_mp3), agregar_url(entry_playlist)))
+    boton_mp3 = ttk.Button(frame_botones, text="Descargar MP3", command=lambda: (ventana_confirmacion(root, validar_enlace(entry_playlist, label_info), formato_mp3)))
     boton_mp3.pack(side=tk.LEFT, padx=10)
 
-    boton_mp4 = ttk.Button(frame_botones, text="Descargar MP4", command=lambda: (ventana_confirmacion(root, validar_enlace(entry_playlist, label_info), formato_mp4), agregar_url(entry_playlist)))
+    boton_mp4 = ttk.Button(frame_botones, text="Descargar MP4", command=lambda: (ventana_confirmacion(root, validar_enlace(entry_playlist, label_info), formato_mp4)))
     boton_mp4.pack(side=tk.LEFT, padx=10)
 
     configurar_botones(boton_mp3, boton_mp4)
