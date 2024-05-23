928fe516443425e503a1f913be4083889073020c
Kevin <andrestorrecilla.14@gmail.com>
2024-05-23 12:11:21 -0500

    fix(Driver): Añadiendo driver desde el archivo global

--- Automatizacion_PyE_coins_discord/Globals.py
+++ Automatizacion_PyE_coins_discord/Globals.py
@@ -1,9 +1,19 @@
-from typing import Optional
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+
+def creacion_driver():
+    options = Options()
+    options.add_argument("--log-level=3")
+
+    Driver = webdriver.Chrome(options=options)
+    Driver.maximize_window()
+    return Driver
+
+
+Driver = creacion_driver()
 
 variables_globales = {
-    "Driver": Optional[webdriver.Chrome],
     "jugadas_ganadas": int(),
     "jugadas_perdidas": int(),
     "jugadas_ganadas_temporales": int(),
